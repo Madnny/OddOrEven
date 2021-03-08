28e2523a7d44320fe6096cb6e619f886636ebb56
Madnny <Oscarzzy555@gmail.com>
2021-03-07 21:21:47 -0600

    OddOrEven_v1

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # This is a sample Python script.
 
+name = input("Enter your name:")
+print("hello " + name + " Please enter a number: ")
+Evaluate = input()
+zero = 0
+def OddOrEven(numbers):
 
-print("hello world")
+    number = int(numbers) % 2
+    if number == zero:
+        return "Odd"
+    else:
+        return "Even"
+result = OddOrEven(Evaluate)
+print(result)
+
+
 
 
